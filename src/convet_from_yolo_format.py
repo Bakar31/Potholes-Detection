e8358f11fc5e2d9577d7e3f4bb2472e3931c8a0d
Bakar31 <abubakar1808031@gmail.com>
2022-12-13 16:41:44 +0600

    image processing

---
+++ src/convet_from_yolo_format.py
@@ -0,0 +1,40 @@
+import os
+import cv2
+import numpy as np
+
+
+text_file_path = 'data/test_labels'
+img_file_path = 'data/test_images'
+
+
+for label in os.listdir(text_file_path):
+    label_name = label.split('.')[0]
+    print(label)
+    ImageID = f'{label_name}.jpg'
+    img = cv2.imread(os.path.join(img_file_path,ImageID))
+    i_h = img.shape[0]
+    i_w = img.shape[1]
+
+    with open(os.path.join(text_file_path,label), 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            line = np.float32(line.split(" "))
+            cx = line[1] * i_w
+            cy = line[2] * i_h
+            w  = line[3] * i_w 
+            h  = line[4] * i_h
+            Conf = line[5]
+            XMin = cx - w/2
+            YMin = cy - h/2
+            XMax = cx + w/2
+            YMax = cy + h/2
+
+            if os.path.exists('test_labels.csv'):
+                with open('test_labels.csv', 'a') as f:
+                    text = "\n" + str(XMin) + ',' +  str(YMin) + ',' +  str(XMax) + ',' +  str(YMax) + ',' +  str(Conf) + ',' + str('pothole') + ',' + str(ImageID)
+                    f.write(text)
+            else:
+                with open('test_labels.csv', 'w') as f:
+                    text =  'XMin' + ',' +  'YMin' + ',' +  'XMax' + ',' +  'YMax' + ',' +  'Conf' + ',' + 'LabelName' + ',' + 'ImageID'
+                    f.write(text)
+                    
---
+++ src/processing.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+import os
+
+def trasform(path):
+    img = cv2.imread(path) 
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((3,3), np.uint8)
+    erosion = cv2.erode(gray_image, kernel, iterations = 1)
+    kernel_sharpening = np.array([[-1,-1,-1], 
+                              [-1,9,-1], 
+                              [-1,-1,-1]])
+    sharpened = cv2.filter2D(erosion, -1, kernel_sharpening)
+    kernel_3x3 = np.ones((3, 3), np.float32) / 9
+    blurred = cv2.filter2D(sharpened, -1, kernel_3x3)
+    # cv2.imshow('blurred', blurred)
+    # cv2.waitKey(0) 
+    return blurred
+
+path= 'pothole-dataset\\train\\images\\train\\'
+val_path= 'pothole-dataset\\train\\images\\val\\'
+test_path = 'pothole-dataset\\test\\images\\'
+destination = 'gray_data\\train\\images\\train\\'
+val_destination = 'gray_data\\train\\images\\val\\'
+test_destination = 'gray_data\\test\\images\\'
+
+for image in os.listdir(test_path):
+    im_path = test_path + image
+    cv2.imwrite(test_destination+image, trasform(im_path))
